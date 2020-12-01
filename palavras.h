795bf09d566c86e2715519ba38384e8e7714b2fb
godstxk <56132788+andrealmeida19@users.noreply.github.com>
2020-12-01 15:38:19 +0000

    Add files via upload

---
+++ palavras.h
@@ -0,0 +1,12 @@
+#ifndef PALAVRAS_H
+#define PALAVRAS_H
+
+	struct quantPalavras;
+	struct palavrasFacil;
+	
+void facil(){
+	const int quantPalavras = 4;
+	char palavrasFacil[4][255] = {"ola", "pinha", "adeus", "escola"};
+}
+
+#endif
